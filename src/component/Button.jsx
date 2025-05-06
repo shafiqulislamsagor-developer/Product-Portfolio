94fd802c2c4c8105ee1e55b1904a0411c47b7e58
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-05-07 00:51:02 +0600

    companies and hero section reusable added

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 
-export default function Button({ text, className = "" }) {
+export default function Button({ text, className = "", type, icon }) {
+  if (type === "outline1") {
+    return (
+      <a
+        href="#"
+        className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+      >
+        {icon} {text}
+      </a>
+    );
+  }
+  if (type === "outline2") {
+    return (
+      <a
+        href="#"
+        className="inline-flex items-center justify-center w-full px-5 py-3 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+      >
+        {icon} {text}
+      </a>
+    );
+  }
   return (
     <a
       href="#"
---
+++ src/component/Description.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Description({ text }) {
+  return (
+    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+      {text}
+    </p>
+  );
+}
