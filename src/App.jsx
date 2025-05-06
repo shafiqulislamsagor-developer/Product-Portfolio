fb9bbda001684a5a18ba3d3120e8f44cfe18cc85
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-05-06 21:24:55 +0600

    component create and apply

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Companies from "./component/Componies";
+import Footer from "./component/Footer";
+import Hero from "./component/Hero";
+import Navbar from "./component/Navbar";
+import Pricing from "./component/Pricing";
+import Services from "./component/Services";
+import Testimonial from "./component/Testimonial";
+import WorldWide from "./component/WorldWide";
+import "./index.css";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+      <Navbar />
+      <Hero />
+      <Companies />
+      <Services />
+      <WorldWide />
+      <Testimonial />
+      <Pricing />
+      <Footer />
+    </div>
+  );
 }
