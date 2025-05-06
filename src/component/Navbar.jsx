79edc1cbbef53edc1c76adcb606e30e26e353ae8
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-05-06 22:17:25 +0600

    navbar reusable completed

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,83 +1,44 @@
 import React from "react";
+import logo from "../assets/logo.svg";
+import Button from "./Button";
+
+function List({ active, text, href }) {
+  return (
+    <li>
+      <a
+        href={href}
+        className={`block py-2 pl-3 pr-4  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 ${
+          active ? "text-white dark:text-white" : "text-gray-700"
+        }`}
+      >
+        {text}
+      </a>
+    </li>
+  );
+}
 
 export default function Navbar() {
   return (
-    <header class="fixed w-full">
-      <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
-        <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-          <a href="#" class="flex items-center">
-            <img
-              src="./assets/logo.svg"
-              class="h-6 mr-3 sm:h-9"
-              alt="Landwind Logo"
-            />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+    <header className="fixed w-full">
+      <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
+        <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+          <a href="#" className="flex items-center">
+            <img src={logo} className="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Learn with Sumit
             </span>
           </a>
-          <div class="flex items-center lg:order-2">
-            <a
-              href="#"
-              target="_blank"
-              class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
-            >
-              Download
-            </a>
+          <div className="flex items-center lg:order-2">
+            <Button text={"Download"} />
           </div>
           <div
-            class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
+            className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
             id="mobile-menu-2"
           >
-            <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Company
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Marketplace
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Features
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Team
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Contact
-                </a>
-              </li>
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              {NavList.map((li, id) => (
+                <List key={id} {...li} />
+              ))}
             </ul>
           </div>
         </div>
@@ -85,3 +46,36 @@ export default function Navbar() {
     </header>
   );
 }
+
+const NavList = [
+  {
+    text: "Home",
+    href: "#",
+    active: true,
+  },
+  {
+    text: "Company",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Marketplace",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Features",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Team",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Contact",
+    href: "#",
+    active: false,
+  },
+];
