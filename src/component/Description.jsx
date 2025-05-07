19e2327a1347fb36295e21a98c952906ac8f4b72
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-05-07 21:47:45 +0600

    footer , pricing and other section reusable added

--- src/component/Description.jsx
+++ src/component/Description.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-export default function Description({ text }) {
+export default function Description({ text, className = "" }) {
   return (
-    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+    <p
+      className={`max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 ${className}`}
+    >
       {text}
     </p>
   );
